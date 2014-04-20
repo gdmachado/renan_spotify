51322855d3a1736cf814062766fcf475a4fb0c25
Gustavo Machado <gdmachado@me.com>
2014-04-20 19:56:12 -0300

    finishing touches

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,14 @@
+class ResultsController < ApplicationController
+	def regexify
+		@input = params[:q].split(/\r?\n/)
+		@string = ""
+		for line in @input
+			begin
+				@regex = /^\s*[1-9]*\.\s*([\s\S\d\D\w\W]*)\s+-\s*([\s\S\d\D\w\W]*)$/.match(line)
+				@string << @regex[2].strip + ' - ' + @regex[1].strip + "\n"
+			rescue	
+				@string << "ERRO\n"
+			end
+		end
+	end
+end
